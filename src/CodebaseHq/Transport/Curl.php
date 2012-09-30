bc89c25b7f68b741df6f7de265d6e62989ffb288
Mateusz Tymek <mtymek@gmail.com>
2012-09-30 11:34:41 +0200

    body for CURL transport

--- src/CodebaseHq/Transport/Curl.php
+++ src/CodebaseHq/Transport/Curl.php
@@ -5,16 +5,39 @@ namespace CodebaseHq\Transport;
 class Curl extends AbstractTransport
 {
 
+    protected $codebaseUrl = 'https://api3.codebasehq.com';
+
     /**
      * Perform API call
      *
-     * @param $endpoint
+     * @param $username
+     * @param $apiKey
+     * @param $api
      * @param string $method
      * @param null $data
      * @return mixed
      */
-    public function call($username, $apiKey, $endpoint, $method = 'GET', $data = null)
+    public function call($username, $apiKey, $api, $method = 'GET', $data = null)
     {
-        // TODO: Implement call() method.
+        $api = '/' . ltrim($api, '/');
+
+        $curl = curl_init($this->codebaseUrl . $api);
+        curl_setopt_array($curl, array(
+            CURLOPT_HTTPHEADER => array(
+                "Accept: application/xml",
+                "Content-type: application/xml",
+            ),
+            CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+            CURLOPT_USERPWD => $username . ':' . $apiKey,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => $method
+        ));
+
+        if (null !== $data) {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+
+        $ret = curl_exec($curl);
+        return $ret;
     }
 }
