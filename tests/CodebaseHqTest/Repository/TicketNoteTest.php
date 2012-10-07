9c6055310454555ea13a14e51a38809495a5c26d
Mateusz Tymek <mtymek@gmail.com>
2012-10-07 13:11:35 +0200

    renames

--- tests/CodebaseHqTest/Repository/TicketNoteTest.php
+++ tests/CodebaseHqTest/Repository/TicketNoteTest.php
@@ -2,52 +2,29 @@
 
 namespace CodebaseHqTest;
 
-use CodebaseHq\Repository\Ticket as TicketRepo;
+use CodebaseHq\Repository\TicketNote as TicketNoteRepo;
+use CodebaseHq\Entity\TicketNote as TicketNoteEntity;
 
-class TicketTest extends \PHPUnit_Framework_TestCase
+class TicketNoteTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testFindCreatesEntitiesFromXml()
+    public function testCreateSendsValidXml()
     {
         $mock = $this->getMock('CodebaseHq\Api', array('api', 'getProject'));
         $mock->expects($this->once())->method('getProject')
             ->will($this->returnValue('project'));
         $mock->expects($this->once())->method('api')
-            ->with('/project/tickets?query=', 'GET')
-            ->will($this->returnValue(
-                    '<tickets><ticket><ticket-id type="integer">1</ticket-id><summary>Foo</summary></ticket>
-                    <ticket><ticket-id type="integer">2</ticket-id><summary>Bar</summary></ticket></tickets>'
-            ));
+            ->with('/project/tickets/199/notes', 'POST',
+'<ticket-note>
+    <content>Lorem Ipsum</content>
+    <time-added></time-added>
+    <changes></changes>
+</ticket-note>');
 
-        $repo = new TicketRepo($mock);
-
-        $tickets = $repo->find();
-
-        $this->assertInstanceOf('CodebaseHq\Entity\Ticket', $tickets[0]);
-        $this->assertEquals(1, $tickets[0]->getId());
-        $this->assertEquals('Foo', $tickets[0]->getSummary());
-        $this->assertInstanceOf('CodebaseHq\Entity\Ticket', $tickets[1]);
-        $this->assertEquals(2, $tickets[1]->getId());
-        $this->assertEquals('Bar', $tickets[1]->getSummary());
-    }
-
-    public function testGetTicket()
-    {
-        $mock = $this->getMock('CodebaseHq\Api', array('api', 'getProject'));
-        $mock->expects($this->once())->method('getProject')
-            ->will($this->returnValue('project'));
-        $mock->expects($this->once())->method('api')
-            ->with('/project/tickets/124', 'GET')
-            ->will($this->returnValue(
-                    '<ticket><ticket-id type="integer">124</ticket-id><summary>Foo</summary></ticket>'
-            ));
-
-        $repo = new TicketRepo($mock);
-        $ticket = $repo->findOneById(124);
-
-        $this->assertInstanceOf('CodebaseHq\Entity\Ticket', $ticket);
-        $this->assertEquals(124, $ticket->getId());
-        $this->assertEquals('Foo', $ticket->getSummary());
+        $repo = new TicketNoteRepo($mock);
+        $entity = new TicketNoteEntity();
+        $entity->setContent('Lorem Ipsum');
+        $repo->create(199, $entity);
     }
 
 }
